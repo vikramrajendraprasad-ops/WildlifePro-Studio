a178cfe3f9f736907ce1e6bff467640fd0869353
vikramrajendraprasad-ops <vikramrajendraprasad@gmail.com>
2026-01-02 17:07:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-
 from kivy.app import App
 from kivy.uix.label import Label
 
-class TestApp(App):
+class LockedSDKApp(App):
     def build(self):
-        return Label(text="SDK LOCKED ✅", font_size="24sp")
+        return Label(
+            text="ANDROID SDK LOCKED ✅",
+            font_size="24sp"
+        )
 
 if __name__ == "__main__":
-    TestApp().run()
+    LockedSDKApp().run()
